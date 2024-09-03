6067d92a256f4af416b5f430786cbe92b3951a90
Vijendran Iyer <vijendraniyer@gmail.com>
2024-09-03 12:10:56 +0530

    Update checker.c
    Updated file for paradigm shift

--- checker.c
+++ checker.c
@@ -2,13 +2,15 @@
 #include <assert.h>
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
+  if (temperature < 0 || temperature > 45) {
     printf("Temperature out of range!\n");
     return 0;
-  } else if(soc < 20 || soc > 80) {
+  }
+  if (soc < 20 || soc > 80) {
     printf("State of Charge out of range!\n");
     return 0;
-  } else if(chargeRate > 0.8) {
+  }
+  if (chargeRate > 0.8) {
     printf("Charge Rate out of range!\n");
     return 0;
   }
